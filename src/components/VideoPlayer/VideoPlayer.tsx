a7fa38146b3fcacf50183dd1e7ae531da869b785
alexhulbert <alex@alexhulbert.com>
2020-11-10 01:15:50 -0500

    Simplified a lot of stuff and added room support

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -13,13 +13,23 @@ import {
 import React from "react";
 import { Form, Formik } from "formik";
 import ReactPlayer from "react-player";
+import { SingletonRouter, withRouter } from "next/router";
 import { InputField } from "../general/InputField";
 import { PlayPauseButton } from "./PlayPauseButton";
 import { VideoQueue } from "./VideoQueue";
+import {
+  subscribeToVideoState,
+  joinRoom,
+  updateStatus,
+  VideoState
+} from '../../api/sync/sync-client';
 
-type VideoPlayerProps = {};
+type VideoPlayerProps = {
+  router: SingletonRouter;
+};
 
 type VideoPlayerState = {
+  ignoreUpdates: boolean;
   playing: boolean;
   seeking: boolean;
   played: number;
@@ -37,12 +47,31 @@ type progressState = {
 class VideoPlayer extends React.Component<VideoPlayerProps, VideoPlayerState> {
   player: ReactPlayer | null = null;
   state: VideoPlayerState = {
+    ignoreUpdates: true,
     playing: false,
     seeking: false,
     played: 0,
     url: "",
     videoUrlQueue: [],
   };
+
+  constructor(props: VideoPlayerProps) {
+    super(props);
+    const queryString = '?' + props.router.asPath.split('?')[1];
+    const code = new URLSearchParams(queryString).get('code');
+    if (code) {
+      joinRoom(parseInt(code, 10));
+      subscribeToVideoState(this.handleSync);
+    } else {
+      // TODO: Error handling (no room code)
+    }
+  }
+
+  handleSync = ({ playing, videoPosition }: VideoState) => {
+    this.setState({ ignoreUpdates: true });
+    this.player?.seekTo(videoPosition / 1000);
+    this.setState({ playing });
+  }
 
   handlePlayPause = () => {
     this.setState({ playing: !this.state.playing });
@@ -63,19 +92,21 @@ class VideoPlayer extends React.Component<VideoPlayerProps, VideoPlayerState> {
     this.setState({ played: state.played * 100 });
   };
 
-  // TODO(issue) connect all callbacks to the sync service.
-  handlePlay = () => {};
-
-  handlePause = () => {};
-
-  handleSeek = () => {};
+  handleUpdate = () => {
+    // TODO: switch to `const { playing, ignoreUpdates } = this.state;` after #3 is fixed
+    if (this.state.ignoreUpdates) return this.setState({ ignoreUpdates: false });
+    const playing = (this.player!.getInternalPlayer() as any).getPlayerState() === 1;
+    const videoPosition = this.player!.getCurrentTime() * 1000;
+    (window as any).player = this.player;
+    updateStatus(playing, videoPosition);
+  };
 
   refPlayer = (player: ReactPlayer | null) => {
     this.player = player;
   };
 
   render() {
-    const { playing, seeking, played, url, videoUrlQueue } = this.state;
+    const { playing, played, url, videoUrlQueue } = this.state;
     return (
       <>
         <Stack justify="space-between" align="stretch">
@@ -132,9 +163,9 @@ class VideoPlayer extends React.Component<VideoPlayerProps, VideoPlayerState> {
               ref={this.refPlayer}
               url={url}
               playing={playing}
-              onPlay={this.handlePlay}
-              onPause={this.handlePause}
-              onSeek={this.handleSeek}
+              onPlay={this.handleUpdate}
+              onPause={this.handleUpdate}
+              onSeek={this.handleUpdate}
               onEnd={this.handleEnd}
               onProgress={this.handleProgress}
             ></ReactPlayer>
@@ -158,4 +189,4 @@ class VideoPlayer extends React.Component<VideoPlayerProps, VideoPlayerState> {
   }
 }
 
-export default VideoPlayer;
+export default withRouter(VideoPlayer);
